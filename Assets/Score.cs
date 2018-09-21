37ef4aec6fa73cce5e62a83493431e86fd40c464
Adhokshith <adatta9@uic.edu>
2018-09-21 13:57:36 -0500

    Commit

---
+++ Assets/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+	public Text scoreText;
+	public int maxScore=5;
+	int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	public void AddPoint () {
+		score++;
+		if (score != maxScore)
+			scoreText.text = "Score: " + score;
+		else
+			scoreText.text = "You won!";
+	}
+}
---
+++ Assets/TargetScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour {
+	public Score scoreManager;
+	private void OnCollisionEnter(Collision collision)
+	{
+		scoreManager.AddPoint ();
+		Debug.Log ("Collision Detected");
+		Destroy (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
